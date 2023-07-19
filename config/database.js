62c30521ba8c370bf6c13c8ac1e668828ae7f5e5
JayrajPratapSingh <jairaj273310@gmail.com>
2023-07-19 19:28:28 +0530

    changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 import mongoose from "mongoose"
-export const ConnectDatabase = ()=> {mongoose.connect(process.env.DATABASE ).then((c)=>{
+const dbURI = process.env.DATABASE
+export const ConnectDatabase = ()=> {mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } ).then((c)=>{
     console.log(`database connected sucessfully: ${c.connection.host}`)
  }).catch((e)=>console.log(e))}
