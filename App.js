77222464eaefaac17e1537500e5097be92ef454e
JayrajPratapSingh <114416289+JayrajPratapSingh@users.noreply.github.com>
2023-07-19 21:58:47 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,14 +3,11 @@ import cookieParser from "cookie-parser"
 export const app = express()
 import cors from "cors"
 app.use("*",cors({
-    origin:true,
+    origin:"https://jps-portfolio-frontend.vercel.app",
     credentials:true,
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+
   
 app.use(express.json({limit:"50mb"}))
 app.use(express.urlencoded({extended:true, limit:"50mb"}))
